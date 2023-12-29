44fcd04910469954169a0c14a4d8abb43ce10e58
kelvin-ndoma <kelvinndomamutua@gmail.com>
2023-12-29 15:35:23 +0300

    afew abcs

--- index.js
+++ index.js
@@ -1,90 +1,45 @@
+// Function to initialize Swiper without autoplay
+function initializeSwiper(selector, slidesPerView) {
+  return new Swiper(selector, {
+      spaceBetween: 30,
+      loop: true,
+      centeredSlides: true,
+      autoplay: false,  // Disable autoplay
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+      },
+      breakpoints: {
+          0: {
+              slidesPerView: 1,
+          },
+          768: {
+              slidesPerView: slidesPerView,
+          },
+          1024: {
+              slidesPerView: slidesPerView,
+          },
+      },
+  });
+}
+
+// Initialize Swiper for product-row
+var productSwiper = initializeSwiper(".product-row", 3);
+
+// Initialize Swiper for blogs-row
+var blogsSwiper = initializeSwiper(".blogs-row", 1);
+
+// Initialize Swiper for review-row
+var reviewSwiper = initializeSwiper(".review-row", 3);
+
+// Your existing code for navbar toggle
 let navbar = document.querySelector('.navbar');
-document.querySelector('#menu-bar').onclick=() =>{
-    navbar.classList.toggle('active');
-}
+document.querySelector('#menu-bar').onclick = () => {
+  navbar.classList.toggle('active');
+};
 
-
-
+// Your existing code for search toggle
 let search = document.querySelector('.search');
-document.querySelector('#search').onclick=() =>{
-    search.classList.toggle('active');
-}
-
-
-var swiper = new Swiper(".product-row", {
-    spaceBetween: 30,
-    loop:true,
-    centeredSlides:true,
-    autoplay:{
-        delay:9500,
-        disableOnInteraction:false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-    },
-  });
-var swiper = new Swiper(".blogs-row", {
-    spaceBetween: 30,
-    loop:true,
-    centeredSlides:true,
-    autoplay:{
-        delay:9500,
-        disableOnInteraction:false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation:{
-        nextE1 :".swiper-button-next",
-        prevE1 :".swiper-button-prev",
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 1,
-      },
-      1024: {
-        slidesPerView: 1,
-      },
-    },
-  });
-
-  var swiper = new Swiper(".review-row", {
-    spaceBetween: 30,
-    loop:true,
-    centeredSlides:true,
-    autoplay:{
-        delay:9500,
-        disableOnInteraction:false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-    },
-  });
+document.querySelector('#search').onclick = () => {
+  search.classList.toggle('active');
+};
